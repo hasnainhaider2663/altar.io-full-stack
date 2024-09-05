c573d06fd734cc0e11773bd3317cc868076dff37
hasnain2663 <hasnainhaider2663@gmail.com>
2024-09-05 14:14:18 +0500

    fix generate function to return the grid.
    Calculate occurences in the test.

--- backend/src/__tests__/generate2dGrid.test.ts
+++ backend/src/__tests__/generate2dGrid.test.ts
@@ -12,7 +12,7 @@ describe("generate2dGrid", () => {
 	});
 
 	it("should generate a 10x10 grid by default", () => {
-		const { grid } = generate2dGrid({ numberOfRowsAndColumns: 10 });
+		const grid = generate2dGrid({ numberOfRowsAndColumns: 10 });
 
 		// Check grid size and content
 		expect(grid).toHaveLength(10);
@@ -22,7 +22,7 @@ describe("generate2dGrid", () => {
 	});
 
 	it("should generate a 7x7 grid", () => {
-		const { grid } = generate2dGrid({ numberOfRowsAndColumns: 7 });
+		const grid = generate2dGrid({ numberOfRowsAndColumns: 7 });
 
 		// Check grid size and content
 		expect(grid).toHaveLength(7);
@@ -34,9 +34,11 @@ describe("generate2dGrid", () => {
 	it("should generate a grid biased towards a single character", () => {
 		const biasCharacter = "z";
 		const biasWeight = 0.2;
-		const { occurrences } = generate2dGrid({ numberOfRowsAndColumns: 10, biasCharacter, biasWeight });
+		const grid = generate2dGrid({ numberOfRowsAndColumns: 10, biasCharacter, biasWeight });
 
+    const flattenedArray= grid.reduce((x,y)=>[...x,...y])
+    const occurrencesOfBiasedCharacter=flattenedArray.filter(x=>x===biasCharacter).length
 		// Check that bias character appears at least 20% of the time
-		expect(occurrences[biasCharacter]).toBeGreaterThanOrEqual(biasWeight * 10);
+		expect(occurrencesOfBiasedCharacter).toBeGreaterThanOrEqual(biasWeight * 10);
 	});
 });
--- backend/src/helpers/generate-2d-grid.ts
+++ backend/src/helpers/generate-2d-grid.ts
@@ -7,18 +7,14 @@ interface IGridProps {
 }
 export default function generate2dGrid({ numberOfRowsAndColumns = 10, biasCharacter, biasWeight }: IGridProps) {
 	const grid = [];
-	const map = new Map();
 	for (let i = 0; i < numberOfRowsAndColumns; i++) {
 		const row = [];
 		for (let j = 0; j < numberOfRowsAndColumns; j++) {
-			const randomCharacter = generateRandomCharactor({biasCharacter, biasWeight});
+			const randomCharacter = generateRandomCharactor({ biasCharacter, biasWeight });
 			row.push(randomCharacter);
-			const occurence = map.get(randomCharacter) || 0;
-			map.set(randomCharacter, occurence + 1);
 		}
 		grid.push(row);
 	}
-	const occurrences = Object.fromEntries(map.entries());
 
-	return { grid, occurrences };
+	return grid;
 }
