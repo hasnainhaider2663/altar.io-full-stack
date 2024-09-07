5fe644e02fd6ab888ea2f597740b00a8977245e9
hasnain2663 <hasnainhaider2663@gmail.com>
2024-09-07 21:48:49 +0500

    fetch new grid on key input

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { AsyncPipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { interval, Observable } from 'rxjs';
 import { GridComponent } from '../components/grid/grid.component';
 import ApiResponse from '../models/api-response.model';
 import { ApiService } from '../services/api/api.service';
@@ -12,13 +12,40 @@ import { ApiService } from '../services/api/api.service';
   standalone: true,
   imports: [GridComponent, AsyncPipe],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   grid$: Observable<ApiResponse> | null = null;
+  inputDisabled = false;
+
   constructor(private apiService: ApiService) {}
+  ngOnInit(): void {
+    interval(4000).subscribe(() => {
+      this.inputDisabled = !this.inputDisabled;
+    });
+  }
 
   generate2dGridClicked() {
-    if (this.grid$ === null) {
-      this.grid$ = this.apiService.generateGrid();
+    this.grid$ = this.apiService.generateGrid();
+  }
+  onCharactedChanged(event: KeyboardEvent) {
+    const regex = /^[a-z]+$/;
+    const { key } = event;
+
+    // Allow special keys
+    if (
+      key === 'Backspace' ||
+      key === 'Tab' ||
+      key === 'Enter' ||
+      key === 'ArrowLeft' ||
+      key === 'ArrowRight' ||
+      key === 'Delete'
+    ) {
+      return;
     }
+    if (!regex.test(key)) {
+      event.preventDefault();
+      return;
+    }
+
+    this.grid$ = this.apiService.generateGrid(key, 0.9);
   }
 }
