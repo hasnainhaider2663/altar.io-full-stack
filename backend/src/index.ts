46a0528e64c3754926d307dbafde7ae4432b75ee
hasnain2663 <hasnainhaider2663@gmail.com>
2024-09-07 15:56:48 +0500

    Added CORS to allow frontend to make requests to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,16 @@
+import cors from 'cors';
 import express from 'express';
 import gridRouter from './controllers/grid-controller';
-
 const app = express();
 const port = process.env.PORT || 3000;
 
+const corsOptions = {
+	origin: 'http://localhost:4200',
+	methods: ['GET', 'POST'],
+};
+
 app.use(express.json());
-app.use(cors())
+app.use(cors(corsOptions));
 
 app.use('/grid', gridRouter);
 
