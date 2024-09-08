e5080904f67a7506799261da42b6f0b58f3e149f
hasnain2663 <hasnainhaider2663@gmail.com>
2024-09-08 23:29:03 +0500

    Form

--- frontend/src/app/payments/payments.component.ts
+++ frontend/src/app/payments/payments.component.ts
@@ -1,5 +1,6 @@
 import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Observable } from 'rxjs';
 import ApiResponse from '../models/api-response.model';
 import { ApiService } from '../services/api/api.service';
@@ -7,14 +8,25 @@ import { ApiService } from '../services/api/api.service';
 @Component({
   selector: 'app-payments',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, ReactiveFormsModule],
   templateUrl: './payments.component.html',
   styleUrl: './payments.component.scss',
 })
 export class PaymentsComponent {
   grid$: Observable<ApiResponse | null>;
-
+  form: FormGroup;
   constructor(private apiService: ApiService) {
     this.grid$ = this.apiService.getGrid();
+
+    this.form = new FormGroup({
+      payment: new FormControl(''),
+      amount: new FormControl(''),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+
+    this.form.reset();
   }
 }
