d09c77b34ca0bcaf6145cddf6b584250c583df47
hasnain2663 <hasnainhaider2663@gmail.com>
2024-09-08 16:44:03 +0500

    Payments component init and Socket.io setup on backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import cors from 'cors';
 import express from 'express';
+import { Server } from 'socket.io';
 import gridRouter from './controllers/grid-controller';
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -10,10 +12,24 @@ const corsOptions = {
 };
 
 app.use(express.json());
+
+const httpServer = app.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`);
+});
+
+const io = new Server(httpServer, {
+	cors: corsOptions,
+});
+
 app.use(cors(corsOptions));
 
 app.use('/grid', gridRouter);
 
-app.listen(port, () => {
-	console.log(`Server running at http://localhost:${port}`);
+io.on('connection', (socket) => {
+	console.log('A user connected');
+
+	// You can handle custom events here
+	socket.on('disconnect', () => {
+		console.log('A user disconnected');
+	});
 });
