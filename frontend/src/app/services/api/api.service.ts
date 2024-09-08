11f994e966a1d7b6a2cebd77885996cdbef28f37
hasnain2663 <hasnainhaider2663@gmail.com>
2024-09-08 19:46:40 +0500

    Unsubscribe from observable on grid updated

--- frontend/src/app/services/api/api.service.ts
+++ frontend/src/app/services/api/api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, interval } from 'rxjs';
+import { BehaviorSubject, interval, Subject, Subscription } from 'rxjs';
 import { io, Socket } from 'socket.io-client';
 import ApiResponse from 'src/app/models/api-response.model';
 
@@ -11,27 +11,35 @@ export class ApiService {
   private socket: Socket;
   private gridSubject = new BehaviorSubject<ApiResponse | null>(null);
   private grid$ = this.gridSubject.asObservable();
-  private biasCharacter?: string;
+  private stopInterval$ = new Subject<void>(); // Subject to control the interval emission
+  private intervalSubscription: Subscription | null = null;
   constructor(private httpClient: HttpClient) {
     this.socket = io('http://localhost:3000', { autoConnect: true });
 
     this.socket.on('grid-updated', (result) => {
       console.log('grid-updated', result);
       this.gridSubject.next(result);
+      this.stopInterval();
     });
-
-    this.grid$.subscribe((x) => (this.biasCharacter = x?.biasCharacter));
   }
   getGrid() {
     return this.grid$;
   }
   generateGrid(biasCharacter?: string, biasWeight?: number) {
-    this.biasCharacter = biasCharacter;
-    interval(2000).subscribe((x) => {
+    this.stopInterval$.next(); // Emit to stop any existing interval
+    this.intervalSubscription = interval(2000).subscribe((x) => {
       this.socket.emit('generate-grid', {
         biasCharacter,
         biasWeight,
       });
     });
+  }
+
+  stopInterval() {
+    console.log('stopping?');
+    if (this.intervalSubscription) {
+      this.intervalSubscription.unsubscribe();
+      this.intervalSubscription = null;
+    }
   }
 }
